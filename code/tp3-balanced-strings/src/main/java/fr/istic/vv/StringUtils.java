5542c4f20c7ee2f6b68b3dd557440e9b73486530
walid <82517052+waldoslastosba@users.noreply.github.com>
2024-01-29 01:18:28 +0100

    Update StringUtils.java

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,46 @@
 package fr.istic.vv;
 
+import java.util.List;
+import java.util.Stack;
+
 public class StringUtils {
 
     private StringUtils() {}
 
+    private final static List<Character> openSymb = List.of('(', '{', '[');
+    private final static List<Character> closeSymb = List.of(')', '}', ']');
+
     public static boolean isBalanced(String str) {
-        return false;
+        Stack<Character> stack = new Stack();
+
+        for (char current : str.toCharArray()) {
+            if (openSymb.contains(current)) {
+                stack.push(current);
+            } else if (!stack.empty() && closeSymb.contains(current) && (stack.peek().equals(getBalancingChar(current)))) {
+                stack.pop();
+            } else return false;
+        }
+
+        return stack.empty();
+    }
+
+    public static char getBalancingChar(char c) {
+        switch (c) {
+            case '(':
+                return ')';
+            case ')':
+                return '(';
+            case '{':
+                return '}';
+            case '}':
+                return '{';
+            case '[':
+                return ']';
+            case ']':
+                return '[';
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
 }
