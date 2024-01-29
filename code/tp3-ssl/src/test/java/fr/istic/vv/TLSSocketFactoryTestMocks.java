1a19418e32b4a0d49e8bea8cb13c69ea13119052
walid <82517052+waldoslastosba@users.noreply.github.com>
2024-01-29 01:22:25 +0100

    Update TLSSocketFactoryTestMocks.java

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -1,6 +1,7 @@
 package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,5 +12,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TLSSocketFactoryTestMocks {
 
+    /**
+     * Test when the edge case when the both supported and enabled protocols are null.
+     */
+    @Test
+    public void preparedSocket_NullProtocols()  {
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket mockSSLSocket = createSSLSocketMock(null, null);
+
+        f.prepareSocket(mockSSLSocket);
+
+        assertNull(mockSSLSocket.getEnabledProtocols());
+        assertNull(mockSSLSocket.getSupportedProtocols());
+
+        Mockito.verify(mockSSLSocket, Mockito.never()).setEnabledProtocols(Mockito.any());
+    }
+
+    @Test
+    public void typical()  {
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket mockSSLSocket = createSSLSocketMock(shuffle(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"}), shuffle(new String[]{"SSLv3", "TLSv1"}));
+
+        f.prepareSocket(mockSSLSocket);
+
+        Mockito.verify(mockSSLSocket, Mockito.atLeastOnce()).setEnabledProtocols(new String[] {"TLSv1.2", "TLSv1.1", "TLSv1", "SSLv3" });
+    }
+
+
+    private String[] shuffle(String[] in) {
+        List<String> list = new ArrayList<String>(Arrays.asList(in));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
+    }
+
+    private SSLSocket createSSLSocketMock(String[] supportedProtocols, String[] enabledProtocols){
+        SSLSocket mockSSLSocket = Mockito.mock(SSLSocket.class);
+
+        Mockito.when(mockSSLSocket.getSupportedProtocols()).thenReturn(supportedProtocols);
+        Mockito.when(mockSSLSocket.getEnabledProtocols()).thenReturn(enabledProtocols);
+
+        return mockSSLSocket;
+    }
 
 }
