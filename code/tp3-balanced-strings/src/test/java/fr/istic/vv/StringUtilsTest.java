c93449025af1a242d0ff2e9ad6ec9f7bec924ae5
walid <82517052+waldoslastosba@users.noreply.github.com>
2024-01-29 01:18:46 +0100

    Update StringUtilsTest.java

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,89 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    @Test
+    void testIsBalancedEmpty() {
+        assertTrue(StringUtils.isBalanced(""));
+    }
 
+    @Test
+    void testIsBalancedOddNumberOfChar() {
+        assertFalse(StringUtils.isBalanced("{}}"));
+    }
+
+    @Test
+    void testIsBalancedNotAllowedChar() {
+        assertFalse(StringUtils.isBalanced("a"));
+    }
+
+    @Test
+    void testIsBalancedFirstCharClosing() {
+        assertFalse(StringUtils.isBalanced("][[]"));
+    }
+
+    @Test
+    void testIsBalancedLastCharOpening() {
+        assertFalse(StringUtils.isBalanced("())("));
+    }
+
+    @Test
+    void testIsBalancedFirstCharClosingAndLastCharOpening() {
+        assertFalse(StringUtils.isBalanced(")("));
+    }
+
+    @Test
+    void testIsBalancedValid1() {
+        assertTrue(StringUtils.isBalanced("()"));
+    }
+
+    @Test
+    void testIsBalancedValid2() {
+        assertTrue(StringUtils.isBalanced("[{}({})]"));
+    }
+
+    @Test
+    void testIsBalancedValid3() {
+        assertTrue(StringUtils.isBalanced("[{}]()"));
+    }
+
+    @Test
+    void testIsBalancedNotValid() {
+        assertFalse(StringUtils.isBalanced("[{]}()"));
+    }
+
+    @Test
+    void testBalancingChar1() {
+        assertEquals(StringUtils.getBalancingChar('('),')');
+    }
+
+    @Test
+    void testBalancingChar2() {
+        assertEquals(StringUtils.getBalancingChar(')'),'(');
+    }
+
+    @Test
+    void testBalancingChar3() {
+        assertEquals(StringUtils.getBalancingChar('['),']');
+    }
+
+    @Test
+    void testBalancingChar4() {
+        assertEquals(StringUtils.getBalancingChar(']'),'[');
+    }
+
+    @Test
+    void testBalancingChar5() {
+        assertEquals(StringUtils.getBalancingChar('{'),'}');
+    }
+
+    @Test
+    void testBalancingChar6() {
+        assertEquals(StringUtils.getBalancingChar('}'),'{');
+    }
+
+
+    @Test
+    void testBalancingChar7() {
+        assertThrows(IllegalArgumentException.class, () -> StringUtils.getBalancingChar('a'));
+    }
 }
